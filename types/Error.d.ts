c6d7746aedb3d4dccedf0977b44ae4cc9ab93383
T. Michael Keesey <keesey@gmail.com>
2018-09-22 14:52:00 -0700

    Made Error.type stricter.

--- types/Error.d.ts
+++ types/Error.d.ts
@@ -1,7 +1,27 @@
+export declare type ErrorType = 'ACCESS_DENIED'
+    | 'API_CONFIGURATION_ERROR'
+    | 'AUTHORIZER_FAILURE'
+    | 'AUTHORIZER_CONFIGURATION_ERROR'
+    | 'BAD_REQUEST_PARAMETERS'
+    | 'BAD_REQUEST_BODY'
+    | 'DEFAULT_4XX'
+    | 'DEFAULT_5XX'
+    | 'EXPIRED_TOKEN'
+    | 'INVALID_SIGNATURE'
+    | 'INTEGRATION_FAILURE'
+    | 'INTEGRATION_TIMEOUT'
+    | 'INVALID_API_KEY'
+    | 'MISSING_AUTHENTICATION_TOKEN'
+    | 'QUOTA_EXCEEDED'
+    | 'REQUEST_TOO_LARGE'
+    | 'RESOURCE_NOT_FOUND'
+    | 'THROTTLED'
+    | 'UNAUTHORIZED'
+    | 'UNSUPPORTED_MEDIA_TYPE';
 export declare interface Error {
     readonly developer_message: string;
     readonly documentation?: string;
     readonly field?: string;
-    readonly type: string;
+    readonly type: ErrorType;
     readonly user_message: string;
 }
