742f7da381e3cc82474ed3d9bffcc1741b176709
T. Michael Keesey <keesey@gmail.com>
2018-12-26 22:44:49 -0800

    Added unit tests for validateDate and checked against invalid but properly formatted dates.

--- validation/validateDate.ts
+++ validation/validateDate.ts
@@ -1,10 +1,17 @@
+import { ValidationFault } from './ValidationFault';
 export default (query: { [name: string]: string; }, field: string) => {
     const value = query[field];
     if (!/^\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(value)) {
         return [{
             field,
             message: `Not a valid ISO datetime: "${value}". Required format is: "YYYY-MM-DDTHH:MM:SS.sssZ".`,
-        }];
+        }] as ReadonlyArray<ValidationFault>;
     }
-    return [];
+    if (isNaN(new Date(value).valueOf())) {
+        return [{
+            field,
+            message: `Not a valid datetime: "${value}".`,
+        }] as ReadonlyArray<ValidationFault>;
+    }
+    return [] as ReadonlyArray<ValidationFault>;
 };
