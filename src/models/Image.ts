6237f32a71918843d6c6776152dfafad3a9b4044
T. Michael Keesey <keesey@gmail.com>
2019-01-29 17:38:27 -0800

    Revisions to Image._links.

--- src/models/Image.ts
+++ src/models/Image.ts
@@ -1,16 +1,18 @@
 import { Entity } from './Entity';
-import { ImagePostLinks } from './ImagePost';
 import { Link } from './Link';
 import { Links } from './Links';
 import { MediaLink } from './MediaLink';
-export declare interface ImageLinks extends ImagePostLinks, Links {
+export declare interface ImageLinks extends Links {
     readonly contributor: Link;
-    readonly 'http://ogp.me/ns#image': MediaLink;
+    readonly generalNode: Link | null;
+    readonly 'http://ogp.me/ns#image': MediaLink | null;
+    readonly license: Link;
     readonly nodes: ReadonlyArray<Link>;
     readonly rasterFiles: ReadonlyArray<MediaLink>;
     readonly sourceFile: MediaLink;
+    readonly specificNode: Link;
     readonly submission: Link;
-    readonly 'twitter:image': MediaLink;
+    readonly 'twitter:image': MediaLink | null;
     readonly vectorLink: MediaLink | null;
 }
 export declare interface Image extends Entity {
